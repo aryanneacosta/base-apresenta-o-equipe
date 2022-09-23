8cea9ccd1d364cff1b700896e05f3f6b7a8015d1
Aryanne Acosta <aryanneacosta@gmail.com>
2022-09-22 21:38:07 -0300

    base de site para uma equipe

---
+++ script.js
@@ -0,0 +1,15 @@
+function redirectInsta() {
+    window.location.href = "https://www.instagram.com/aryanneacosta/";
+}
+
+function redirectGitHub() {
+    window.location.href = "https://github.com/aryanneacosta";
+}
+
+function redirectLinkedIn()  {
+    window.location.href = "https://www.linkedin.com/in/aryanne-acosta/";
+}
+
+function redirectEmail() {
+    window.location.href = "mailto:aryanneacosta@gmail.com";
+}
